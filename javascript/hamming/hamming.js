ba04b525fd08b3a36792604b5a84dd6c0e16b738
Matt Brock <mbrock8@gmail.com>
2017-01-30 14:02:21 -0600

    refactor hamming code using a guard clause to check for incorrect input

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,21 +4,20 @@ var Hamming = function () {};
 
 Hamming.prototype.compute = function (strandA, strandB) {
 	
-	if (strandA.length === strandB.length) {
-		var i, d = 0;
-		
-		for (i = 0; i < strandA.length; i++) {
-			d = strandA[i] !== strandB[i] ? d + 1 : d;
-		}
-		
-		return d;
-		
-	} else {
+	var i, d = 0;
+	
+	if (strandA.length !== strandB.length) {
 		throw {
 			name: 'Error',
 			message: 'DNA strands must be of equal length.'
 		}
 	}
+	
+	for (i = 0; i < strandA.length; i++) {
+		d = strandA[i] !== strandB[i] ? d + 1 : d;
+	}
+	
+	return d;
 }
 
 module.exports = Hamming;
