cb9d55007166e18daecf741a70f7572ae5d63321
Matt Brock <mbrock8@gmail.com>
2017-01-31 15:20:46 -0600

    refactor bob.js a bit to clean up

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,32 +3,30 @@ var Bob = function () {};
 
 Bob.prototype.hey = function (input) {
 	
-	var responses = {
-		question: 'Sure.',
-		yelling: 'Whoa, chill out!',
-		silence: 'Fine. Be that way!',
-		other: 'Whatever.'
-	};
-	
-	var i = input;
-	
-	function onlyNumbers (str, delim) {
-		var chars = str.split(delim) || chars;
-		
-		for (var j = 0; j < chars.length; j++) {
-			if (chars[j] != parseInt(chars[j])) return false;
-		}
-		
-		return true;
-	}
+	var i = input,
+			responses = {
+				question: 'Sure.',
+				yelling: 'Whoa, chill out!',
+				silence: 'Fine. Be that way!',
+				other: 'Whatever.'
+			},
+			onlyNumbers = function (str, delim) {
+				var chars = str.split(delim) || chars;
+				
+				for (var j = 0; j < chars.length; j++) {
+					if (chars[j] != parseInt(chars[j])) return false;
+				}
+				
+				return true;
+			};
 	
 	// input is empty or only whitespace
 	if (i.trim().length === 0) return responses.silence;
 	
 	// input is a comma-separated string of numbers
-	if (onlyNumbers (i, ', ')) return responses.other;
+	if (onlyNumbers(i, ', ')) return responses.other;
 	
-	// input was only numbers in a question
+	// input is only numbers in a question
 	if (onlyNumbers(i.split('?')[0])) return responses.question;
 	
 	// input was uppercase to being with
@@ -36,6 +34,8 @@ Bob.prototype.hey = function (input) {
 	
 	// input ends with a ?
 	if (i[i.length - 1] === '?') return responses.question;
+	
+	// input is anything else
 	return responses.other;
 	
 }
