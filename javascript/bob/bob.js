e23adc6f9142fdac06933fc78d7b396525c8a7e2
Matt Brock <mbrock8@gmail.com>
2017-01-31 15:17:05 -0600

    finish bob problem and submit

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,9 +2,42 @@
 var Bob = function () {};
 
 Bob.prototype.hey = function (input) {
-	var response = '';
 	
-	return response;
+	var responses = {
+		question: 'Sure.',
+		yelling: 'Whoa, chill out!',
+		silence: 'Fine. Be that way!',
+		other: 'Whatever.'
+	};
+	
+	var i = input;
+	
+	function onlyNumbers (str, delim) {
+		var chars = str.split(delim) || chars;
+		
+		for (var j = 0; j < chars.length; j++) {
+			if (chars[j] != parseInt(chars[j])) return false;
+		}
+		
+		return true;
+	}
+	
+	// input is empty or only whitespace
+	if (i.trim().length === 0) return responses.silence;
+	
+	// input is a comma-separated string of numbers
+	if (onlyNumbers (i, ', ')) return responses.other;
+	
+	// input was only numbers in a question
+	if (onlyNumbers(i.split('?')[0])) return responses.question;
+	
+	// input was uppercase to being with
+	if (i === i.toUpperCase()) return responses.yelling;
+	
+	// input ends with a ?
+	if (i[i.length - 1] === '?') return responses.question;
+	return responses.other;
+	
 }
 
 module.exports = Bob;
